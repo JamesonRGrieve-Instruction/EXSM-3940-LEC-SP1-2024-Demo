020836879cb250ccf49ea2b25e0c00c42b0ed91f
James Grieve <jgrieve@ualberta.ca>
2024-04-17 19:39:55 -0600

    Middleware update to show only the page and not static files;

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,5 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
-  // This log will only run on the server, because it is in middleware which the client never sees.
-  // The same is true of server-only components (ones without 'use client').
-  console.log(request.nextUrl.pathname);
-  /* Example of Redirect
-  if (request.nextUrl.pathname !== '/') {
-    return NextResponse.redirect(new URL('/', request.url));
+export default function middleware(request) {
+  if (!request.nextUrl.pathname.startsWith('/_next/')) {
+    console.log(request.nextUrl.pathname);
   }
-  */
 }
