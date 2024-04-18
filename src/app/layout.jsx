cd0adc729b62469b2d4bf810ad2952a305dfb3a1
James Grieve <jgrieve@ualberta.ca>
2024-04-17 19:49:32 -0600

    Client-side routing example;

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+// Don't mix this up with the MUI Link unless you inject the NextJS Link functionality therein via theming, etc.
+import Link from 'next/link';
 import React from 'react';
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +14,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href='/'>Home</Link>
+              </li>
+              <li>
+                <Link href='/about'>About</Link>
+              </li>
+              <li>
+                <Link href='/about/me'>About Me</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <main>{children}</main>
       </body>
     </html>
