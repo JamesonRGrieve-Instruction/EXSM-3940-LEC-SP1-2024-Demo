7cac3854f9f49c9c9928490ab693fd8bd97d1875
James Grieve <jgrieve@ualberta.ca>
2024-04-18 19:41:44 -0600

    Stateful field into push;

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,18 +2,20 @@
 import { useRouter } from 'next/navigation';
 import styles from './page.module.css';
 import aboutStyles from './about.module.css';
-import React from 'react';
+import React, { useState } from 'react';
 export default function Home({ searchParams }) {
   console.log('On Page: ', searchParams);
   const router = useRouter();
+  const [name, setName] = useState('');
   return (
     <>
       {/* <h1 className={`${styles.about_heading} ${aboutStyles.about_headings}`}>About Page</h1> */}
       <h1 className={[styles.about_heading, aboutStyles.about_headings].join(' ')}>About Page</h1>
+      <input type='text' value={name} onChange={(event) => setName(event.target.value)} />
       <button
         onClick={(event) => {
           event.preventDefault();
-          router.push('/?name=Joe');
+          router.push(`/?name=${name.trim()}`);
         }}
       >
         Go Home
