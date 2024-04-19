cf27134c36df4832d6f35bd03cb4fb12ef811327
James Grieve <jgrieve@ualberta.ca>
2024-04-18 19:45:58 -0600

    Non-stateful form field into push;

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,24 +2,24 @@
 import { useRouter } from 'next/navigation';
 import styles from './page.module.css';
 import aboutStyles from './about.module.css';
-import React, { useState } from 'react';
+import React from 'react';
 export default function Home({ searchParams }) {
   console.log('On Page: ', searchParams);
   const router = useRouter();
-  const [name, setName] = useState('');
   return (
     <>
       {/* <h1 className={`${styles.about_heading} ${aboutStyles.about_headings}`}>About Page</h1> */}
       <h1 className={[styles.about_heading, aboutStyles.about_headings].join(' ')}>About Page</h1>
-      <input type='text' value={name} onChange={(event) => setName(event.target.value)} />
-      <button
-        onClick={(event) => {
+      <form
+        onSubmit={(event) => {
           event.preventDefault();
-          router.push(`/?name=${name.trim()}`);
+          const name = event.target.elements.name.value.trim();
+          router.push(`/?name=${name}`);
         }}
       >
-        Go Home
-      </button>
+        <input type='text' name='name' defaultValue='' />
+        <button type='submit'>Go Home</button>
+      </form>
     </>
   );
 }
