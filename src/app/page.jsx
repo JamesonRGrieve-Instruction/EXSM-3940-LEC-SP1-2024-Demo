7b4b642e5c399f2ef773e289e2b29f0e9da4d974
James Grieve <jgrieve@ualberta.ca>
2024-04-24 19:38:13 -0600

    Fonts object;

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,23 @@
 import React, { Suspense } from 'react';
 import SearchParamRenderer from './SearchParamRender';
 import { Box, Typography } from '@mui/material';
-import { Josefin_Sans } from 'next/font/google';
+import { Josefin_Sans, EB_Garamond } from 'next/font/google';
 
 const josefinSans = Josefin_Sans({
   weight: ['100', '200', '300', '600', '700'],
   subsets: ['latin'],
   style: ['normal', 'italic'],
 });
+const ebGaramond = EB_Garamond({
+  weight: ['600', '700'],
+  subsets: ['latin'],
+  style: ['normal', 'italic'],
+});
+
+const fonts = {
+  josefinSans: josefinSans,
+  ebGaramond: ebGaramond,
+};
 
 export default function Home() {
   // This log will run on both server and client, because it is in the body of a client component.
@@ -15,10 +25,10 @@ export default function Home() {
   return (
     <>
       <Box component='main'>
-        <Typography variant='h1' fontWeight='600' fontStyle='italic' className={josefinSans.className}>
+        <Typography variant='h1' fontWeight='600' fontStyle='italic' className={fonts.josefinSans.className}>
           Home
         </Typography>
-        <Typography variant='h1' className={josefinSans.className}>
+        <Typography variant='h1' className={fonts.ebGaramond.className}>
           Page
         </Typography>
       </Box>
