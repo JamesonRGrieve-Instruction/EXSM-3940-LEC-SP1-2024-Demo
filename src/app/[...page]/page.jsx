43a26ef947d10785f93c9d10720a5b767ea32cb8
James Grieve <jgrieve@ualberta.ca>
2024-04-24 19:23:15 -0600

    Fill layout image;

--- src/app/[...page]/page.jsx
+++ src/app/[...page]/page.jsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import Image from 'next/image';
 import ducksImage from '../../../public/img/ducks.jpeg';
 import React from 'react';
@@ -9,6 +9,9 @@ export default function Home({ params }) {
       <Typography variant='h1'>About {params.page.join('/')} Page</Typography>
       <Image src={ducksImage} alt='A bunch of rubber ducks.' width='1280' height='853' placeholder='blur' />
       <Image src='https://cdn.pixabay.com/photo/2017/02/21/16/30/mexico-2086549_1280.jpg' alt='A turtle.' width='1280' height='960' />
+      <Box width='30rem' height='20rem' position='relative'>
+        <Image src='https://cdn.pixabay.com/photo/2020/03/09/02/22/jellyfish-4914197_1280.jpg' alt='A turtle.' layout='fill' objectFit='contain' />
+      </Box>
     </>
   );
 }
