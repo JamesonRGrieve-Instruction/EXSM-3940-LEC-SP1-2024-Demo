1a6c779e6b761bb13dac4512c7fbc474585880ca
James Grieve <jgrieve@ualberta.ca>
2024-05-01 19:29:47 -0600

    Solve hydration error;

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,15 +1,31 @@
+'use client';
 import { Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import AboutForm from './AboutForm';
 
 export default function Home({ searchParams }) {
   console.log('On Page: ', searchParams);
+  const [client, setClient] = useState(false);
+  React.useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setClient(true);
+    }
+  }, []);
   return (
     <>
       {/* <h1 className={`${styles.about_heading} ${aboutStyles.about_headings}`}>About Page</h1> */}
       <Typography variant='h1' textAlign='center'>
         About Page
       </Typography>
+      {client ? (
+        <Typography variant='body1' textAlign='center'>
+          This is the about page on mobile!
+        </Typography>
+      ) : (
+        <Typography variant='h5' textAlign='center'>
+          This is the about page on desktop!
+        </Typography>
+      )}
       <AboutForm />
     </>
   );
