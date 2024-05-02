c224013c3559b55fcdffd6709b5039da7ff0bc45
James Grieve <jgrieve@ualberta.ca>
2024-05-01 20:17:26 -0600

    Statically rendered dynamic routes;

--- src/app/about/[productCategory]/[product]/page.jsx
+++ src/app/about/[productCategory]/[product]/page.jsx
@@ -10,3 +10,33 @@ export default function Home({ params }) {
     </>
   );
 }
+export async function generateStaticParams() {
+  return [
+    {
+      productCategory: 'electronics',
+      product: 'laptop',
+    },
+    {
+      productCategory: 'electronics',
+      product: 'phone',
+    },
+    {
+      productCategory: 'electronics',
+      product: 'tablet',
+    },
+    {
+      productCategory: 'clothing',
+      product: 'shirt',
+    },
+    {
+      productCategory: 'clothing',
+      product: 'pants',
+    },
+    {
+      productCategory: 'clothing',
+      product: 'hat',
+    },
+  ];
+}
+// If the user navigates to a paramset that is not defined, fall back to the catch all (or 404 if not present), rather than dynamically rendering.
+export const dynamicParams = false;
