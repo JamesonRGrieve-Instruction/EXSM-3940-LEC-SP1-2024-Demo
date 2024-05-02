4f97b9de3f3b094dc4e794cab3937e871b21747f
James Grieve <jgrieve@ualberta.ca>
2024-05-01 19:30:40 -0600

    Flip hydration error-causing code back to server-side;

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,23 +1,16 @@
-'use client';
 import { Typography } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 import AboutForm from './AboutForm';
 
 export default function Home({ searchParams }) {
   console.log('On Page: ', searchParams);
-  const [client, setClient] = useState(false);
-  React.useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setClient(true);
-    }
-  }, []);
   return (
     <>
       {/* <h1 className={`${styles.about_heading} ${aboutStyles.about_headings}`}>About Page</h1> */}
       <Typography variant='h1' textAlign='center'>
         About Page
       </Typography>
-      {client ? (
+      {typeof window !== 'undefined' ? (
         <Typography variant='body1' textAlign='center'>
           This is the about page on mobile!
         </Typography>
