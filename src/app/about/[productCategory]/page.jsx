6df7457e985744a66e8ab23d6dd4722c52900567
James Grieve <jgrieve@ualberta.ca>
2024-05-02 19:12:38 -0600

    Generating metadata with a function (with params and searchParams);

--- src/app/about/[productCategory]/page.jsx
+++ src/app/about/[productCategory]/page.jsx
@@ -1,5 +1,10 @@
 import { Typography } from '@mui/material';
 import React from 'react';
+export function generateMetadata({ params, searchParams }) {
+  return {
+    title: 'Category - ' + params.productCategory + ' | ' + searchParams['title'],
+  };
+}
 export default function Home({ params }) {
   console.log('On Page: ', params);
   return (
