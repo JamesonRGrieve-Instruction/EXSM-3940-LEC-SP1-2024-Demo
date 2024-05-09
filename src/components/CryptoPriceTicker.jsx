7230addbe97c48e32453e7aca7ce7425045a9dba
James Grieve <jgrieve@ualberta.ca>
2024-05-08 19:51:48 -0600

    Error display;

--- src/components/CryptoPriceTicker.jsx
+++ src/components/CryptoPriceTicker.jsx
@@ -4,7 +4,7 @@ import useSWR from 'swr';
 import axios from 'axios';
 import { Typography } from '@mui/material';
 export default function CryptoPriceTicker({ assetName }) {
-  const { data } = useSWR(
+  const { data, error } = useSWR(
     `assets/${assetName}`,
     async () => {
       return (await axios.get(`https://api.coincap.io/v2/assets/${assetName}`)).data.data;
@@ -17,9 +17,18 @@ export default function CryptoPriceTicker({ assetName }) {
       },
     },
   );
+  console.log(error);
   return (
-    <Typography variant='h1'>
-      1 {data.symbol} = ${Number(data.priceUsd).toFixed(2)} USD
-    </Typography>
+    <>
+      <Typography variant='h1'>
+        1 {data.symbol} = ${Number(data.priceUsd).toFixed(2)} USD
+      </Typography>
+      {error && (
+        <Typography variant='body1' color='firebrick'>
+          {error.response.status}: {JSON.stringify(error.response.data)}
+        </Typography>
+      )}
+      ;
+    </>
   );
 }
