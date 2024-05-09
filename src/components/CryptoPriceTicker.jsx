e27e348f53597a63dae8fa25a3884b5884803ff3
James Grieve <jgrieve@ualberta.ca>
2024-05-08 19:42:53 -0600

    Show loading even on revalidation;

--- src/components/CryptoPriceTicker.jsx
+++ src/components/CryptoPriceTicker.jsx
@@ -4,18 +4,18 @@ import useSWR from 'swr';
 import axios from 'axios';
 import { Typography } from '@mui/material';
 export default function CryptoPriceTicker({ assetName }) {
-  const { data: data } = useSWR(`assets/${assetName}`, async () => {
+  const { data, isValidating } = useSWR(`assets/${assetName}`, async () => {
     return (await axios.get(`https://api.coincap.io/v2/assets/${assetName}`)).data.data;
     // One of the .data is to pull the axios response body out, the other is because this particular API nests the data in a "data" object.
   });
   return (
     <Typography variant='h1'>
-      {data ? (
+      {isValidating ? (
+        <>Loading...</>
+      ) : (
         <>
           1 {data.symbol} = ${Number(data.priceUsd).toFixed(2)} USD
         </>
-      ) : (
-        <>Loading...</>
       )}
     </Typography>
   );
