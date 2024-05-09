9fc7647676cd7cac4336cc1ae57e62e87d89fdc5
James Grieve <jgrieve@ualberta.ca>
2024-05-08 20:09:11 -0600

    Additional config options; Move formatting into SWR to reduce rerenders;

--- src/components/CryptoPriceTicker.jsx
+++ src/components/CryptoPriceTicker.jsx
@@ -7,12 +7,23 @@ export default function CryptoPriceTicker({ assetName }) {
   const { data, error } = useSWR(
     `assets/${assetName}`,
     async () => {
-      return (await axios.get(`https://api.coincap.io/v2/assets/${assetName}`)).data.data;
+      const toReturn = (await axios.get(`https://api.coincap.io/v2/assets/${assetName}`)).data.data;
+      toReturn.priceUsd = Number(toReturn.priceUsd).toFixed(2);
       // One of the .data is to pull the axios response body out, the other is because this particular API nests the data in a "data" object.
     },
     {
-      revalidateOnFocus: false,
-      revalidateOnMount: false,
+      revalidateOnFocus: false, // When you click off the window and back.
+      focusThrottleInterval: 5000, // Only focus revalidate once every X ms.
+      revalidateOnMount: false, // When the component mounts.
+      revalidateOnReconnect: false, // When you get a network connection back.
+      // refreshInterval: 1000,
+      refreshWhenHidden: false, // When minimized.
+      refreshWhenOffline: false, // When no network (browser dependent).
+      shouldRetryOnError: false, // Don't keep trying if an error is returned.
+      errorRetryInterval: 3000, // How often to retry.
+      errorRetryCount: 10, // Retry up to X times.
+      dedupingInterval: 2000, // Requests are deduped if multiple on the same key in X ms (except mutate).
+      loadingTimeout: 3000, // Timeout if no response.
       fallbackData: {
         symbol: 'Asset',
         priceUsd: 0,
@@ -22,7 +33,7 @@ export default function CryptoPriceTicker({ assetName }) {
   return (
     <>
       <Typography variant='h1'>
-        1 {data.symbol} = ${Number(data.priceUsd).toFixed(2)} USD
+        1 {data.symbol} = ${data.priceUsd} USD
       </Typography>
       <Button
         variant='contained'
