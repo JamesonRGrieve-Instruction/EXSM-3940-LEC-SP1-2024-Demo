6cb46952a1fb601d76c9dc860531324d7bd20c7c
James Grieve <jgrieve@ualberta.ca>
2024-05-08 19:55:10 -0600

    Manual revalidation with mutate;

--- src/components/CryptoPriceTicker.jsx
+++ src/components/CryptoPriceTicker.jsx
@@ -1,8 +1,8 @@
 'use client';
 import React from 'react';
-import useSWR from 'swr';
+import useSWR, { mutate } from 'swr';
 import axios from 'axios';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 export default function CryptoPriceTicker({ assetName }) {
   const { data, error } = useSWR(
     `assets/${assetName}`,
@@ -11,18 +11,27 @@ export default function CryptoPriceTicker({ assetName }) {
       // One of the .data is to pull the axios response body out, the other is because this particular API nests the data in a "data" object.
     },
     {
+      revalidateOnFocus: false,
+      revalidateOnMount: false,
       fallbackData: {
         symbol: 'Asset',
         priceUsd: 0,
       },
     },
   );
-  console.log(error);
   return (
     <>
       <Typography variant='h1'>
         1 {data.symbol} = ${Number(data.priceUsd).toFixed(2)} USD
       </Typography>
+      <Button
+        variant='contained'
+        onClick={() => {
+          mutate(`assets/${assetName}`);
+        }}
+      >
+        Revalidate
+      </Button>
       {error && (
         <Typography variant='body1' color='firebrick'>
           {error.response.status}: {JSON.stringify(error.response.data)}
