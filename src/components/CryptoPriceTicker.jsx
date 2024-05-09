6637e1a03002c69d2892fd3892f520171a902032
James Grieve <jgrieve@ualberta.ca>
2024-05-08 19:46:06 -0600

    Fallback data for loading (and errors);

--- src/components/CryptoPriceTicker.jsx
+++ src/components/CryptoPriceTicker.jsx
@@ -4,19 +4,22 @@ import useSWR from 'swr';
 import axios from 'axios';
 import { Typography } from '@mui/material';
 export default function CryptoPriceTicker({ assetName }) {
-  const { data, isValidating } = useSWR(`assets/${assetName}`, async () => {
-    return (await axios.get(`https://api.coincap.io/v2/assets/${assetName}`)).data.data;
-    // One of the .data is to pull the axios response body out, the other is because this particular API nests the data in a "data" object.
-  });
+  const { data } = useSWR(
+    `assets/${assetName}`,
+    async () => {
+      return (await axios.get(`https://api.coincap.io/v2/assets/${assetName}`)).data.data;
+      // One of the .data is to pull the axios response body out, the other is because this particular API nests the data in a "data" object.
+    },
+    {
+      fallbackData: {
+        symbol: 'Asset',
+        priceUsd: 0,
+      },
+    },
+  );
   return (
     <Typography variant='h1'>
-      {isValidating ? (
-        <>Loading...</>
-      ) : (
-        <>
-          1 {data.symbol} = ${Number(data.priceUsd).toFixed(2)} USD
-        </>
-      )}
+      1 {data.symbol} = ${Number(data.priceUsd).toFixed(2)} USD
     </Typography>
   );
 }
